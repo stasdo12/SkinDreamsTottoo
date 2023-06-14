8a27aca9d565334744372fcb28028f7d5704ffcc
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-06-14 14:45:18 +0300

    Initial commit

---
+++ src/main/java/com/ua/SkinDreamsTottoo/SkinDreamsTottoo/SkinDreamsTottooApplication.java
@@ -0,0 +1,13 @@
+package com.ua.SkinDreamsTottoo.SkinDreamsTottoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinDreamsTottooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinDreamsTottooApplication.class, args);
+	}
+
+}
