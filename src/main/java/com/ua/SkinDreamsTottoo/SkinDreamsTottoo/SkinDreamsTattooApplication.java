0680eba4c3c9428d1352c28e67a59b132b280314
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-07-03 13:56:28 +0300

    add EmailSenderService

--- src/main/java/com/ua/SkinDreamsTottoo/SkinDreamsTottoo/SkinDreamsTattooApplication.java
+++ src/main/java/com/ua/SkinDreamsTottoo/SkinDreamsTottoo/SkinDreamsTattooApplication.java
@@ -1,20 +1,28 @@
 package com.ua.SkinDreamsTottoo.SkinDreamsTottoo;
 
-import com.ua.SkinDreamsTottoo.SkinDreamsTottoo.services.EmailSenderService;
 import com.ua.SkinDreamsTottoo.SkinDreamsTottoo.util.ClientValidator;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import java.util.Properties;
 
 
 @SpringBootApplication
 public class SkinDreamsTattooApplication {
-	@Autowired
-	private EmailSenderService senderService;
+
+	@Value("${spring.mail.host}")
+	private String mailHost;
+	@Value("${spring.mail.port}")
+	private int mailPort;
+	@Value("${spring.mail.username}")
+	private String mailUsername;
+	@Value("${spring.mail.password}")
+	private String mailPassword;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkinDreamsTattooApplication.class, args);
@@ -29,6 +37,23 @@ public class SkinDreamsTattooApplication {
 	@Bean
 	public ClientValidator clientValidator() {
 		return new ClientValidator();
+	}
+
+	@Bean
+	public JavaMailSender javaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost(mailHost);
+		mailSender.setPort(mailPort);
+		mailSender.setUsername(mailUsername);
+		mailSender.setPassword(mailPassword);
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
 	}
 
 }
